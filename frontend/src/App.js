be9eea651700f319c9c939897e50c314020f26de
Eldarramos <128025562+Eldarramos@users.noreply.github.com>
2023-04-14 22:53:33 -0600

    Combobox en Formulario

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import Login from './Login';
 import Header from './Header';
+import CompBlogs from './component/SBlogs'
+import CompEditBlog from './component/EditBlogs'
+
+
 
 import Home from './component/Home';
 import Formulario from './component/Form';
@@ -13,8 +17,12 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Login/>} />
-          <Route path="/home" element={<Home/>} />
+          <Route path="/Home" element={<Home/>} />
           <Route path='/Formulario' element={<Formulario/>}/>
+
+          <Route path='/CompBlogs' element={<CompBlogs/>}/>
+          <Route path='/edit/:id' element={<CompEditBlog/>}/>   
+
         </Routes>
       </div>
     </BrowserRouter>
