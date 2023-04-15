e97d93880f4edadb9dcfa391e28d99ae76cbcae5
Eldarramos <128025562+Eldarramos@users.noreply.github.com>
2023-04-14 22:47:34 -0600

    Actualizacion de backend

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,22 @@ import cors from 'cors'
 import db from './database/db.js'
 
 import formRoutes from './routes/routes.js'
+import routerStates from './routes/routesState.js'
+//12 abril
+import routerCities from './routes/routesCities.js'
+import routerColonies from './routes/routesColony.js'
 
 const app = express ()
 
 app.use(cors())
 app.use(express.json())
 app.use('/forms', formRoutes)
+//11 abril
+app.use('/states', routerStates)
+//12 abril
+app.use('/cities', routerCities)
+
+app.use('/colonies',routerColonies)
 
 try {
     await db.authenticate()
